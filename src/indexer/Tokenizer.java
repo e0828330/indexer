add172ba996eac134d1066a5cede80bf370c7524
Adel Gadllah <adel.gadllah@gmail.com>
2013-04-13 13:19:06 +0200

    tokenizer: Fix warning

--- src/indexer/Tokenizer.java
+++ src/indexer/Tokenizer.java
@@ -3,7 +3,6 @@ package indexer;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
